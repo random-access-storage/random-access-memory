4b196912fe21143313f6c77a82324c7a1493705a
Paul Frazee <pfrazee@gmail.com>
2016-12-29 12:19:47 -0600

    add a pool of buffers so that RAMs are persistent across instances

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var AbstractRandomAccess = require('abstract-random-access')
 
 module.exports = RandomAccessMemory
 
+var pool = {}
+
 function RandomAccessMemory (buf) {
-  if (!(this instanceof RandomAccessMemory)) return new RandomAccessMemory(buf)
+  if (!(this instanceof RandomAccessMemory)) {
+    if (typeof buf === 'string' && buf in pool) return pool[buf]
+    return new RandomAccessMemory(buf)
+  }
   AbstractRandomAccess.call(this)
   this.buffer = (buf && Buffer.isBuffer(buf)) ? buf : new Buffer(0)
   this.length = this.buffer.length
+  if (typeof buf === 'string') pool[buf] = this
 }
 
 inherits(RandomAccessMemory, AbstractRandomAccess)
