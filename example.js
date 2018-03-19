5f660bb9c2a74579d0f9dc431a067cc13a9edec3
Mathias Buus <mathiasbuus@gmail.com>
2018-03-19 14:50:44 +0100

    update pkg json and standard

--- example.js
+++ example.js
@@ -3,5 +3,5 @@ const ram = require('./index2')
 const mem = ram()
 
 mem.write(0, Buffer.from('hello world'), function () {
-  mem.read(0, 11, (err, data) => console.log(data.toString()))
+  mem.read(0, 11, (_, data) => console.log(data.toString()))
 })
